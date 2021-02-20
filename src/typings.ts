7e5b93192dd0a99f5ca1dd3bb0715b47d1c3d52e
Maf <dragoncatm@gmail.com>
2021-02-20 15:08:45 +0100

    Added typings

--- src/typings.ts
+++ src/typings.ts
@@ -9,7 +9,7 @@ export enum ContextID {
 
 export declare namespace Core {
     namespace block_get_course_blocks {
-        interface args {
+        type args = {
             courseid: string;
         }
 
@@ -36,8 +36,175 @@ export declare namespace Core {
 
     }
 
+    namespace calendar_create_calendar_events {
+        type args = {
+            events: {
+                name: string
+                description: string
+                format?: number //description format (1 = HTML, 0 = MOODLE, 2 = PLAIN or 4 = MARKDOWN)
+                courseid?: number
+                groupid?: number
+                repeats?: number  //number of repeats
+                eventtype?: string //Event type
+                timestart?: number //timestart
+                timeduration?: number//time duration
+                visible?: number //visible
+                sequence?: number //sequence
+            }
+        }
+
+        type reponse = {
+            events: {
+                id: number,
+                name: string,
+                description: string,
+                format: number,
+                courseid: number,
+                groupid: number,
+                userid: number,
+                repeatid: number,
+                modulename: string,
+                instance: number,
+                eventtype: string,
+                timestart: number,
+                timeduration: number,
+                visible: number,
+                uuid: string,
+                sequence: number,
+                timemodified: number,
+                subscriptionid: number,
+            }[],
+            warnings: {
+                item: string,
+                itemid: number,
+                warningcode: string,
+                message: string,
+            }[]
+        }
+    }
+
+    namespace calendar_delete_calendar_events {
+        type args = {
+            eventid: number   //Event ID
+            repeat: 0 | 1   //Delete comeplete series if repeated event
+        }
+
+        type response = {
+
+        }
+    }
+
+    namespace calendar_get_action_events_by_course {
+        type args = {
+            courseid: string;
+            timesortfrom?: number;
+            timesortto?: number;
+            aftereventid?: number;
+            /** @default 20*/
+            limitnum?: number;
+        }
+
+        type reponse = {
+            events: {
+                id: number,
+                name: string,
+                description: string,
+                descriptionformat: number,
+                categoryid: number,
+                groupid: number,
+                userid: number,
+                repeatid: number,
+                eventcount: number,
+                modulename: string,
+                instance: number,
+                eventtype: string,
+                timestart: number,
+                timeduration: number,
+                timesort: number,
+                visible: number,
+                timemodified: number,
+                icon: {
+                    key: string,
+                    component: string,
+                    alttext: string
+                },
+                category: {
+                    id: number,
+                    name: string,
+                    idnumber: string,
+                    description: string,
+                    parent: number,
+                    coursecount: number,
+                    visible: number,
+                    timemodified: number,
+                    depth: number,
+                    nestedname: string,
+                    url: string
+                },
+                course: {
+                    id: number,
+                    fullname: string,
+                    shortname: string,
+                    idnumber: string,
+                    summary: string,
+                    summaryformat: number,
+                    startdate: number,
+                    enddate: number,
+                    fullnamedisplay: string,
+                    viewurl: string
+                },
+                subscription: {
+                    displayeventsource: number,
+                    subscriptionname: string,
+                    subscriptionurl: string
+                },
+                canedit: number,
+                candelete: number,
+                deleteurl: string,
+                editurl: string,
+                viewurl: string,
+                formattedtime: string,
+                isactionevent: number,
+                iscourseevent: number,
+                iscategoryevent: number,
+                groupname: string,
+                url: string,
+                action: {
+                    name: string,
+                    url: string,
+                    itemcount: number,
+                    actionable: number,
+                    showitemcount: number
+                },
+            }[],
+            firstid: number,
+            lastid: number
+        }
+    }
+
+    namespace test4 {
+        type args = {
+            courseid: string;
+        }
+
+        type response = {
+
+        }
+    }
+
+    namespace test5 {
+        type args = {
+            courseid: string;
+        }
+
+        type response = {
+
+        }
+    }
+
+
     namespace webservice_get_site_info {
-        interface response {
+        type response = {
             sitename: string;
             username: string;
             firstname: string;
@@ -69,7 +236,7 @@ export declare namespace Core {
     }
 
     namespace core_course_get_contents {
-        interface args {
+        type args = {
             courseid: string;
         }
 
@@ -84,7 +251,7 @@ export declare namespace Core {
             modules: Module[];
         }[]
 
-        interface Module {
+        type Module = {
             id: number;
             url: string;
             name: string;
@@ -98,7 +265,7 @@ export declare namespace Core {
             description?: string;
         }
 
-        interface Content {
+        type Content = {
             type: 'file' | 'url';
             filename: string;
             filepath?: string;
